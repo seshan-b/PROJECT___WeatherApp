1ea035605c323f205fb893c5307d6d2f8c9b4913
seshan-b <seshan.b@gmail.com>
2022-03-15 12:15:14 +1300

    Edited forecast.js

--- forecast.js
+++ forecast.js
@@ -3,3 +3,18 @@
 // email: seshan.b@gmail.com
 
 const apiKey = 'ifINfoGkGEN5EjqVkuAUkzBbr4nqLVTH'
+
+// Get weather information 
+const getWeather = async (locationId) => {
+
+    const base = 'http://dataservice.accuweather.com/currentconditions/v1/'
+    const query = `${locationId}?apikey=${apiKey}`
+
+    const getResponse = await fetch(base + query)
+    const data = await getResponse.json()
+
+    console.log('Response data: ', data)
+
+    return data[0]
+
+}
