725066db74b6197148ce2d13960faf7e87cfd638
seshan-b <seshan.b@gmail.com>
2022-03-18 11:47:16 +1300

    Edited app.js

--- scripts/app.js
+++ scripts/app.js
@@ -5,3 +5,5 @@ const card = document.querySelector('.card')
 const details = document.querySelector('.details')
 
 const time = document.querySelector('img.time')
+
+const icon = document.querySelector('.icon img')
